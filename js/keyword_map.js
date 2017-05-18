1c1e326b7217e5ef5697cee880bbb6a82f67a2d9
vichara <vichara@vichara-PC>
2017-05-18 16:07:10 +0900

    z

--- js/keyword_map.js
+++ js/keyword_map.js
@@ -1,12 +1,27 @@
+var scripts = document.getElementsByTagName('script');
+var myScript = scripts[ scripts.length - 1 ];
 
-// 지도를 생성합니다    
-var map = new daum.maps.Map(mapContainer, mapOption); 
+var queryString = myScript.src.replace(/^[^\?]+\??/,'');
+
+var params = parseQuery( queryString );
+
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+var infowindow = new daum.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new daum.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 생성합니다
+var map = new daum.maps.Map(mapContainer, mapOption);
 
 // 장소 검색 객체를 생성합니다
-var ps = new daum.maps.services.Places(); 
+var ps = new daum.maps.services.Places();
 
 // 키워드로 장소를 검색합니다
-ps.keywordSearch('부천고속버스터미널', placesSearchCB); 
+ps.keywordSearch(params.q, placesSearchCB);
 
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 function placesSearchCB (status, data, pagination) {
@@ -18,23 +33,23 @@ function placesSearchCB (status, data, pagination) {
 
         for (var i=0; i<data.places.length; i++) {
 			if(i == 0) {
-				displayMarker(data.places[i]);    
+				displayMarker(data.places[i]);
 				bounds.extend(new daum.maps.LatLng(data.places[i].latitude, data.places[i].longitude));
 			}
-        }       
+        }
 
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
         map.setBounds(bounds);
-    } 
+    }
 }
 
 // 지도에 마커를 표시하는 함수입니다
 function displayMarker(place) {
-    
+
     // 마커를 생성하고 지도에 표시합니다
     var marker = new daum.maps.Marker({
         map: map,
-        position: new daum.maps.LatLng(place.latitude, place.longitude) 
+        position: new daum.maps.LatLng(place.latitude, place.longitude)
     });
 
     // 마커에 클릭이벤트를 등록합니다
